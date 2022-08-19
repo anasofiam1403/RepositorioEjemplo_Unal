b74e41db53726583617b3cc48e5e2c5e7d9754f5
anasofiam1403 <ansmolina42@gmail.com>
2022-08-18 20:49:50 -0500

    P3 version inicial

---
+++ src/repositorio_/Productos.java
@@ -0,0 +1,15 @@
+
+package repositorio_;
+
+
+public class Productos {
+    private String nombre;
+    private int precio;
+    private String categoria;
+    
+    public Productos(String nombre, int precio, String categoria){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
+}
--- src/repositorio_/Repositorio_.java
+++ src/repositorio_/Repositorio_.java
@@ -1,20 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package repositorio_;
 
-/**
- *
- * @author user
- */
+
 public class Repositorio_ {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        System.out.println("version inicial");
+        System.out.println("segunda version");
+           }
     
 }
